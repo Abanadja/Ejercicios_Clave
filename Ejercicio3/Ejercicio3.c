2a8712b38d02074c9a3c91fed01d55110e070551
Abanadja <aabanaba@myuax.com>
2024-11-22 00:56:09 +0100

    Ejercicio 2

---
+++ Ejercicio3/Ejercicio3.c
@@ -0,0 +1,18 @@
+//
+// Created by alexa on 22/11/2024.
+//
+
+#include "Ejercicio3.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "Ejercicio1.h"
+
+struct Estudiante copiarEstudiante(struct Estudiante est) {
+    struct Estudiante nuevoEst;
+    strcpy(nuevoEst.nombre, est.nombre);
+    nuevoEst.edad = est.edad;
+    nuevoEst.promedio = est.promedio;
+    return nuevoEst;
+}
---
+++ Ejercicio3/Ejercicio3.h
@@ -0,0 +1,9 @@
+//
+// Created by alexa on 22/11/2024.
+//
+#ifndef EJERCICIO3_H
+#define EJERCICIO3_H
+
+struct Estudiante copiarEstudiante(struct Estudiante est);
+
+#endif //EJERCICIO3_H
