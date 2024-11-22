6d38e2555c6e50d508622bec5e104015694be04f
Abanadja <aabanaba@myuax.com>
2024-11-22 01:01:14 +0100

    main

---
+++ main.c
@@ -0,0 +1,19 @@
+//
+// Created by alexa on 22/11/2024.
+//
+#include "Ejercicio1/Ejercicio1.h"
+#include "Ejercicio2/Ejercicio2.h"
+#include "Ejercicio3/Ejercicio3.h"
+#include "Ejercicio4/Ejercicio4.h"
+#include "Ejercicio5/Ejercicio5.h"
+
+
+int main() {
+
+    Ejercicio1();
+    Ejercicio2();
+    //Ejercicio3 es solo un constructor
+    Ejercicio4();
+    Ejercicio5();
+    return 0;
+}
